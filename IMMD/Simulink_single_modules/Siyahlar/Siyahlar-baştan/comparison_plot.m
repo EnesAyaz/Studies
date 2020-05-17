3e2775e0a5e8a2e8c9d771048aecba735a88b96b
enes <enes.ayaz.1996@gmail.com>
2020-05-17 03:49:17 +0300

    Taking a data from simulink parasitic model
    there are 4 simulations with parasitic 0.1 ohm, 50 ohm and without parasitic 0.1 ohm,50 ohm. Time domain input current, leg current and capacitor current were compared

---
+++ IMMD/Simulink_single_modules/Siyahlar/Siyahlar-baştan/comparison_plot.m
@@ -0,0 +1,55 @@
+load('Y:\Github\DATA\Comparison\siyahlar\Data_with_parasitic50ohm');
+input_current_with=InputCurr;
+SAIA_with=SAIA;
+SBIB_with=SBIB;
+SCIC_with=SCIC;
+capA_cur_with =capA_cur;
+capB_cur_with=capB_cur;
+capC_cur_with=capC_cur;
+
+load('Y:\Github\DATA\Comparison\siyahlar\Data_without_parasitic50ohm');
+input_current_without=InputCurr;
+SAIA_without=SAIA;
+SBIB_without=SBIB;
+SCIC_without=SCIC;
+capA_cur_without =capA_cur;
+capB_cur_without=capB_cur;
+capC_cur_without=capC_cur;
+%%
+figure();
+plot(time,input_current_with,'b-','Linewidth',1);
+hold on;
+plot(time,input_current_without,'r-','Linewidth',1);
+xlim([0.02144 0.02152])
+ylabel('Current(A)','FontSize',20,'FontWeight','Bold')
+xlabel('time(sec)','FontSize',20,'FontWeight','Bold')
+legend({'With parasitic','Without parasitic'},'Location','best');
+title('Input Current ','FontSize',20,'FontWeight','Bold');
+set(gca,'FontSize',20)
+
+%%
+figure();
+plot(time,SAIA_with,'b-','Linewidth',1);
+hold on;
+plot(time,SAIA_without,'r-','Linewidth',1);
+xlim([0.02144 0.02152])
+ylabel('Current(A)','FontSize',20,'FontWeight','Bold')
+xlabel('time(sec)','FontSize',20,'FontWeight','Bold')
+legend({'With parasitic','Without parasitic'},'Location','best');
+title('Leg Current- A ','FontSize',20,'FontWeight','Bold');
+set(gca,'FontSize',20)
+%%
+figure();
+plot(time,capC_cur_with,'b-','Linewidth',1);
+hold on;
+plot(time,capC_cur_without,'r-','Linewidth',1);
+% xlim([0.02144 0.02152])
+ylabel('Current(A)','FontSize',20,'FontWeight','Bold')
+xlabel('time(sec)','FontSize',20,'FontWeight','Bold')
+legend({'With parasitic','Without parasitic'},'Location','best');
+title('Capacitor Current -C ','FontSize',20,'FontWeight','Bold');
+set(gca,'FontSize',20)
+%%
+
+
+
--- IMMD/Simulink_single_modules/Siyahlar/Siyahlar-baştan/param_single_module_parasitic.m
+++ IMMD/Simulink_single_modules/Siyahlar/Siyahlar-baştan/param_single_module_parasitic.m
@@ -15,8 +15,6 @@ ESRA = 21.1e-3;
 ESRB = 21.1e-3;
 ESRC = 21.1e-3;
 
-%% PWM
-
 %% Parasitics
 % Tüm parazitikler var
 LATop  = 03.40e-9;
@@ -33,19 +31,45 @@ LABBot = 10.20e-9;
 LBCTop = 21.84e-9;
 LBCBot = 11.14e-9;
 
-% RATop  = 03.40e-9;
-% RABot  = 20.25e-9;
-% RAMid  = 01.30e-9;
-% RBTop  = 03.38e-9;
-% RBBot  = 20.38e-9;
-% RBMid  = 01.30e-9;
-% RCTop  = 03.41e-9;
-% RCBot  = 20.94e-9;
-% RCMid  = 01.30e-9;
-% RABTop = 18.30e-9;
-% RABBot = 10.20e-9;
-% RBCTop = 21.84e-9;
-% RBCBot = 11.14e-9;
+RATop  = 854e-6;
+RABot  = 6645e-6;
+RAMid  = 218.8e-6*2;
+RBTop  = 813e-6;
+RBBot  = 664e-6;
+RBMid  = 218e-6*2;
+RCTop  = 854e-6;
+RCBot  = 664e-6;
+RCMid  = 218.3e-6*2;
+
+RABTop = 1037e-6-611e-6;
+RABBot = 1422e-6-662e-6;
+RBCTop = 867e-6-611e-6;
+RBCBot = 662e-6-595e-6;
+
+
+%%
+
+% Tüm parazitikler var
+% ACapToDrain = 7.14e-9;
+% ACapToSource = 5.65e-9;
+% ASourceToLoad = 3.724e-9;
+% ADrainToLoad = 3.724e-9;
+% APosToCap = 29.8e-9;
+% ANegToCap = 48.6e-9;
+% % % 
+% BCapToDrain = 7.233e-9;
+% BCapToSource = 5.434e-9;
+% BSourceToLoad = 3.933e-9;
+% BDrainToLoad = 3.933e-9;
+% BPosToCap = 2.63e-9;
+% BNegToCap = 3.183e-9;
+% % % 
+% CCapToDrain = 7.145e-9;
+% CCapToSource = 5.4e-9;
+% CSourceToLoad = 3.242e-9;
+% CDrainToLoad = 3.242e-9;
+% CPosToCap = 43.665e-9;
+% CNegToCap = 31.2e-9;
 
 %% Load parameters
 Vln1 = 0.612*Vdc/sqrt(3)*ModulationIndex1;
@@ -59,8 +83,9 @@ Lload1 = Xload1/(2*pi*ffund);
 Iload1 = Vln1/Z1;
 P = P_module1;
 Idc = P/Vdc;
-Rin = 20; % former value was 10
-Lin = 1e-4;
+
+Rin = 0.1; % former value was 10
+Lin = 1e-6;
 
 Vin = Vdc + Idc*Rin;
 
