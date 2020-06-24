43af11ca8ba65e828992440fb35f531d4084a31d
enes <enes.ayaz.1996@gmail.com>
2020-06-24 13:58:51 +0300

    the leg current is validated for some case by simulink
    Different cases will be tried and reported as well

--- IMMD/Switching_Function/convolution_test.m
+++ IMMD/Switching_Function/convolution_test.m
@@ -3,9 +3,9 @@ carrier_phase=pi;
 sampleTime=1e-8;
 ModulationIndex=0.9;
 ffund=50;
-fundemental_phase=2*pi/3;
+fundemental_phase=-2*pi/3;
 Phase_Current=10;
-load_phase=pi/2;
+load_phase=pi/2+pi/3;
 sim('switching_simulink',0.04);
 %%
 time=Phase_current.time;
@@ -39,12 +39,12 @@ phase_current_FFT_mag(abs(phase_current_FFT_mag)<threshold)=0;
 % ylabel('|P1(f)|')
 % xlim([0 200])
 %%
-% figure();
-% plot(f,180*phase_current_FFT_angle/pi) 
-% title('Single-Sided Amplitude Spectrum of X(t)')
-% xlabel('f (Hz)')
-% xlim([0 300])
-% ylabel('Phase <Angle')
+figure();
+plot(f,180*phase_current_FFT_angle/pi) 
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+xlim([0 300])
+ylabel('Phase <Angle')
 %%
 L=length(leg_current);
 leg_current_FFT_mag=fft(leg_current);
@@ -191,11 +191,11 @@ switchingFunc_FFT_mag(abs(switchingFunc_FFT_mag)<threshold)=0;
 % ylabel('Phase <Angle')
 
 %%
-
+load_phase=load_phase-pi/2+fundemental_phase;
 f_new_left=f-ffund;
 f_new_left = [f_new_left f(end)-ffund/2  f(end) f(end)+ffund/2 f(end)+ffund];
 leg_current_left_mag= [(switchingFunc_FFT_mag*Phase_Current)/2 ;0 ;0 ;0 ; 0];
-leg_current_left_angle=[(switchingFunc_FFT_angle-load_phase-fundemental_phase+pi/2) ;0 ;0 ;0 ;0];
+leg_current_left_angle=[(switchingFunc_FFT_angle-load_phase) ;0 ;0 ;0 ;0];
 
 
 %%
@@ -203,7 +203,7 @@ leg_current_left_angle=[(switchingFunc_FFT_angle-load_phase-fundemental_phase+pi
 f_new_right=f+ffund;
 f_new_right = [-ffund -ffund/2 0 ffund/2 f_new_right ];
 leg_current_right_mag= [0;0;0;0; (switchingFunc_FFT_mag*Phase_Current)/2];
-leg_current_right_angle=[0;0;0;0;(switchingFunc_FFT_angle+load_phase+fundemental_phase-pi/2)];
+leg_current_right_angle=[0;0;0;0;(switchingFunc_FFT_angle+load_phase)];
 
 
 
@@ -218,17 +218,17 @@ leg_current_real_angle =angle(leg_current_right_mag.*exp(1i*leg_current_right_an
 
 
 %%
-figure();
-stem(f,leg_current_FFT_mag,'r') 
-hold on; 
-stem(f_new_right,leg_current_real_mag,'--b') 
-title('Single-Sided Amplitude Spectrum of X(t)')
-xlabel('f (Hz)')
-ylabel('|P1(f)|')
+% figure();
+% stem(f,leg_current_FFT_mag,'r') 
+% hold on; 
+% stem(f_new_right,leg_current_real_mag,'--b') 
+% title('Single-Sided Amplitude Spectrum of X(t)')
+% xlabel('f (Hz)')
+% ylabel('|P1(f)|')
+% % xlim([-100 300])
 % xlim([-100 300])
-xlim([-100 300])
-xlim([39750 40250])
-xlim([79500 80500])
+% xlim([39750 40250])
+% xlim([79500 80500])
 % xlim([119500 120500])
 %%
 figure();
@@ -246,11 +246,126 @@ xlim([-100 300])
 % % xlim([0 300])
 % ylabel('Phase <Angle')
 
+%%
+
+%%
+k=ModulationIndex; % modulation index
+f_o=ffund; % output frequcency(fundamental)
+f_c=fsw; % carrier frequency (switching)
+The_o=-pi/2+fundemental_phase; % output phase
+The_c=pi+carrier_phase-0.0552; % carrier phase
+m_max=8; % maximum harmonics of carrier
+n_max=20; % The number of sidebands
+
+[f_1, Mag, Phase] = SwitchingFunction(k,f_o,f_c,The_o,The_c,m_max,n_max);
+
+f_new= 0:f_o:max(f_1);
+y=(f_1/f_o)+1;
+Mag_new=zeros(1,length(f_new));
+Phase_new=zeros(1,length(f_new));
+
+for i=1:length(y)
+Mag_new((y(i))) =Mag(i);
+Phase_new((y(i)))=Phase(i);
+end
+
+I_mag=Phase_Current;
+% I_phase=load_phase+fundemental_phase+pi/2;
+I_phase=load_phase;
 
 
 
+Mag_0=Mag_new(1);
+f_new=f_new(2:end);
+Mag_new=Mag_new(2:end);
+Phase_new=Phase_new(2:end);
+
+f_new_left_2=f_new-f_o;
+f_new_left_2 = [f_new_left_2 f_new(end) f_new(end)+f_o];
+Mag_new_left= [(Mag_new*I_mag)/2 0 0 ];
+Phase_new_left=[(Phase_new-I_phase) 0 0 ];
+
+
+f_new_right_2= f_new+f_o;
+f_new_right_2 = [0 f_o f_new_right_2];
+Mag_new_right= [0 0 (Mag_new*I_mag)/2  ];
+Phase_new_right=[0 0 (Phase_new+I_phase) ];
+
+Mag_resultant= abs(Mag_new_right.*exp(1i*Phase_new_right)  +  Mag_new_left.*exp(1i*Phase_new_left)) ;          
+Phase_resultant =angle(Mag_new_right.*exp(1i*Phase_new_right)  +  Mag_new_left.*exp(1i*Phase_new_left));
+Mag_resultant(1)= Mag_new_left(1)*cos(Phase_new_left(1));
+
+% Phase_resultant=mod(Phase_resultant,2*pi);        
+% f_new_right_2=f_new_right_2(2:end);
+% Mag_resultant(3)=abs(Mag_resultant(1).*exp(1i*Phase_resultant(1))  +  Mag_resultant(3).*exp(1i*Phase_resultant(3)));
+% Phase_resultant(3)=angle(Mag_resultant(1).*exp(1i*Phase_resultant(1))  +  Mag_resultant(3).*exp(1i*Phase_resultant(3)));
+% Mag_resultant=Mag_resultant(2:end);
+% Phase_resultant=Phase_resultant(2:end);
+Mag_resultant(2)=Mag_0*I_mag;
+Phase_resultant(2)=I_phase;
+
+%%
+figure();
+stem(f,leg_current_FFT_mag,'r') 
+hold on; 
+stem(f_new_right_2,Mag_resultant,'--b') 
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+xlim([-100 300])
+% xlim([0 300])
+xlim([39750 40250])
+xlim([79500 80500])
+% xlim([119500 120500])
+
+%%
+threshold=max(abs(Mag_resultant)/100);
+Phase_resultant(abs(Mag_resultant)<threshold)=0; % determines the low-amplitude threshold
 
 
 
+figure();
+stem(f,180*leg_current_FFT_angle/pi,'r') 
+hold on; 
+stem(f_new_right_2,180*Phase_resultant/pi,'--b') 
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('Angle')
+% % xlim([-100 300])
+xlim([-100 300])
+xlim([39750 40250])
+% xlim([79500 80500])
+% xlim([119500 120500])
+
+%%
+
+threshold=max(abs(Mag)/100);
+Phase(abs(Mag)<threshold)=0; % determines the low-amplitude threshold
+
+figure();
+stem(f_1,180*Phase/pi,'r') 
+hold on; 
+stem(f,180*switchingFunc_FFT_angle/pi,'--b') 
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('Angle')
+xlim([-100 300])
+xlim([-100 300])
+xlim([39750 40250])
+% xlim([79500 80500])
+% xlim([119500 120500])
+
+%%
+% 
+figure();
+stem(f_1,180*Mag/pi,'r') 
+hold on; 
+hold on; 
+stem(f,180*switchingFunc_FFT_mag/pi,'--b') 
+% xlim([-100 300])
+xlim([-100 300])
+xlim([39750 40250])
+xlim([79500 80500])
+xlim([119500 120500])
 
 
