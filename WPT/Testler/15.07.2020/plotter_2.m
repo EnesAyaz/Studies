8729e9dbd620668e31ad16158a85f8b14d5d972a
enes <enes.ayaz.1996@gmail.com>
2020-07-16 19:03:32 +0300

    WPT, decoupled test data are taken and plotted

---
+++ WPT/Testler/15.07.2020/plotter_2.m
@@ -0,0 +1,44 @@
+load('FA150.mat');
+figure();
+
+subplot(3,1,1)
+yyaxis left;
+plot(timeTx,IinVin*20)
+ylabel('Input FB Voltage (V)')
+xlabel('time')
+hold on;
+yyaxis right;
+plot(timeTx,Iin*5)
+ylabel(' Transmitter Current (A)')
+xlabel('time')
+xlim([-1e-5, 1e-5])
+ylim([-10 10 ])
+set(gca,'fontSize',18)
+
+subplot(3,1,2)
+yyaxis left;
+plot(timeRx1,Is1Vin*20)
+ylabel('Input FB Voltage (V)')
+xlabel('time')
+hold on;
+yyaxis right;
+plot(timeRx1,Is1*5)
+ylabel(' Receiver-1 Current (A)')
+xlabel('time')
+xlim([-1e-5, 1e-5])
+ylim([-6 6])
+set(gca,'fontSize',18)
+
+subplot(3,1,3)
+yyaxis left;
+plot(timeRx2,Is2Vin*20)
+ylabel('Input FB Voltage (V)')
+xlabel('time')
+hold on;
+yyaxis right;
+plot(timeRx2,Is2*5)
+ylabel(' Receiver-2 Current (A)')
+xlabel('time')
+xlim([-1e-5, 1e-5])
+ylim([-6 6 ])
+set(gca,'fontSize',18)
