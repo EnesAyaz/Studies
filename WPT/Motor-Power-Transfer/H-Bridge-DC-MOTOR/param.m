e445523c8bb26cb549f0138829cba21adfd0ad4d
Enes <enes.ayaz.1996@gmail.com>
2021-01-20 16:02:24 +0300

    The second harmonic problem
    The second harmonic has been always present in voltage waveform and IPT coils filters them out.  Howver, the bifurcation phenomena occur, or the quality is very low ??? the questions arise in this point.

--- WPT/Motor-Power-Transfer/H-Bridge-DC-MOTOR/param.m
+++ WPT/Motor-Power-Transfer/H-Bridge-DC-MOTOR/param.m
@@ -1,6 +1,6 @@
 
 %% Simulation parameters
-Tfinal=0.20;
+Tfinal=100/40e3;
 SampleTime=1e-7;
 %%
 fsw=40e3;
@@ -8,13 +8,14 @@ SampleTime=(1/fsw)/360;
 phasePWM=180;
 %%
 wr=fsw*2*pi;
+fsw=40e3;
 %% DC BUS
 Vdc=100;
 C_DC=1e-6;
 
 %% PARASITICs
-LSource= 1e-7;
-RSource= 1e-3;
+LSource= 1e-6;
+RSource= 0.1;
 LCAHA= 1e-7;
 LHACB= 1e-7;
 LCBHB= 1e-7;
@@ -22,15 +23,16 @@ COUT=100e-6;
 ESRA=1e-7;
 ESRB=1e-7;
 %% LOAD 
-Rload=5;
+Rload=40;
 % LLoad=500e-6;
-LLoad=2e-3;
+LLoad=1e-3;
 
 %% WPT
 Lp=120e-6;
 Ls=120e-6;
 M=24e-6;
 RL=12;
-Cp=1/(4*wr^2*Lp);
-Cs=1/(4*wr^2*Ls);
+RL2=60;
+Cp=1/(wr^2*Lp);
+Cs=1/(wr^2*Ls);
 
