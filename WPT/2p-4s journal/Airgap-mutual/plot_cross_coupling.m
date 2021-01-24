33afaf41a6a73f524ee9719621cd02a03a843b9b
Enes <enes.ayaz.1996@gmail.com>
2021-01-24 12:14:03 +0300

    H-Bridge model is updated

--- WPT/2p-4s journal/Airgap-mutual/plot_cross_coupling.m
+++ WPT/2p-4s journal/Airgap-mutual/plot_cross_coupling.m
@@ -23,17 +23,18 @@ seconder2=[seconder1(10:end),seconder1(1:9)];
 seconder3=[seconder2(10:end),seconder2(1:9)];
 seconder4=[seconder3(10:end),seconder3(1:9)];
 
-seconder1=[seconder1(1:end),seconder1(end)];
-seconder2=[seconder2(1:end),seconder2(end)];
-seconder3=[seconder3(1:end),seconder3(end)];
-seconder4=[seconder4(1:end),seconder4(end)];
+seconder1=[seconder1(1:end),seconder1(1)];
+seconder2=[seconder2(1:end),seconder2(1)];
+seconder3=[seconder3(1:end),seconder3(1)];
+seconder4=[seconder4(1:end),seconder4(1)];
 
 figure()
-p1=plot(bended_angle(1:end),bended_p1_total(1:end),'-pr','LineWidth',1.5);
-p1.Color(4) = 0.5;
-hold on;
+
 p2=plot(bended_angle(1:end),bended_p2_total(1:end),'-pg','LineWidth',1.5);
 p2.Color(4) = 0.5;
+hold on;
+p1=plot(bended_angle(1:end),bended_p1_total(1:end),'-pr','LineWidth',1.5);
+p1.Color(4) = 0.5;
 hold on;
 plot(bended_angle(1:end),seconder1,'-ok','LineWidth',2);
 hold on;
@@ -78,18 +79,26 @@ bended_p1_total1=[bended_p1_total1,bended_p1_total1(1)];
 bended_p2_total1=[bended_p2_total1,bended_p2_total1(1)];
 %% primer grafiği böyle olucak
 figure()
+p3=plot(bended_angle(1:end),[bended_p1_total(18:end-1),bended_p1_total(1:17),...
+     bended_p1_total(18)],'-pb','LineWidth',1.5);
+p3.Color(4) = 0.5;
+hold on;
+
+p4=plot(bended_angle(1:end),  [bended_p1_total(27:end-1),bended_p1_total(1:26),...
+    bended_p1_total(27) ],'-pg','LineWidth',1.5);
+p4.Color(4) = 0.5;
+hold on;
+
 p1=plot(bended_angle(1:end),bended_p1_total(1:end),'-pm','LineWidth',1.5);
 p1.Color(4) = 0.5;
 hold on;
-p2=plot(bended_angle(1:end),[bended_p1_total(9:end),bended_p1_total(1:8)],'-pr','LineWidth',1.5);
-hold on;
+
+p2=plot(bended_angle(1:end),[bended_p1_total(9:end-1),bended_p1_total(1:8), ...
+    bended_p1_total(9)],'-pr','LineWidth',1.5);
+
 p2.Color(4) = 0.5;
-p3=plot(bended_angle(1:end),[bended_p1_total(18:end),bended_p1_total(1:17)],'-pb','LineWidth',1.5);
 hold on;
-p3.Color(4) = 0.5;
-p4=plot(bended_angle(1:end),  [bended_p1_total(27:end),bended_p1_total(1:26)],'-pg','LineWidth',1.5);
-hold on;
-p4.Color(4) = 0.5;
+
 plot(bended_angle(1:end),bended_p1_total1,'k','LineWidth',2);
 hold on;
 plot(bended_angle(1:end),bended_p2_total1,'ko','LineWidth',1);
